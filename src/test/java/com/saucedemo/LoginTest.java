6f71ca5b8bd028e8d7e302808670971fae34d2bb
TanyaTD24 <td.lar@ya.ru>
2026-01-24 17:49:45 +0300

    add new BaseTest and annotations testng

--- src/test/java/com/saucedemo/LoginTest.java
+++ src/test/java/com/saucedemo/LoginTest.java
@@ -1,22 +1,15 @@
 package com.saucedemo;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.testng.annotations.Test;
 
+public class LoginTest extends BaseTest {
 
-public class LoginTest extends BaseClass {
-    public static void main(String[] args) {
-
-        LoginTest loginTest = new LoginTest();
-        loginTest.setupDriver();
-        loginTest.getBaseUrl();
-        loginTest.assertTitle();
-        loginTest.input_userName();
-        loginTest.input_userPassword();
-        loginTest.clickButtonLogin();
-        loginTest.assertTitleProductPage();
-        loginTest.closeWindow();
+    @Test
+    public void testLogin() {
+        assertTitle();
+        input_userName();
+        input_userPassword();
+        clickButtonLogin();
+        assertTitleProductPage();
     }
-
 }
